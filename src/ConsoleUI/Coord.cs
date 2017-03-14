6d6fbcf3dbd680eba68355de5b365633f33f261d
Jon Seaman <jonmseaman@live.com>
2017-03-14 00:40:55 -0400

    Move subclasses from NativeMethods

---
+++ src/ConsoleUI/Coord.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ConsoleUI
+{
+    public static partial class NativeMethods
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Coord
+        {
+            public short X;
+            public short Y;
+
+            public Coord(short X, short Y)
+            {
+                this.X = X;
+                this.Y = Y;
+            }
+        };
+    }
+}
---
+++ src/ConsoleUI/SmallRect.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ConsoleUI
+{
+    public static partial class NativeMethods
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SmallRect
+        {
+            public short Left;
+            public short Top;
+            public short Right;
+            public short Bottom;
+        }
+    }
+}
